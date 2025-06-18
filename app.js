705988bed431ee0b5449a11a396aa3af49d81a2d
Corobo <81382526+TheCoroboCorner@users.noreply.github.com>
2025-06-18 12:12:09 -0400

    repeat import removed

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ import express from 'express';
 import fetch from 'node-fetch';
 import FormData from 'form-data';
 import path from 'path';
-import path from 'path';
 import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
